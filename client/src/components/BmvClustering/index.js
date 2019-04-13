cc205ee1aca1e626ab7cdca0ab8885a50f39da93
Scott McDonald <scottocorp@gmail.com>
2019-04-13 18:50:57 +1000

    replace map.addLayer with <Layer>

--- client/src/components/BmvClustering/index.js
+++ client/src/components/BmvClustering/index.js
@@ -75,55 +75,6 @@ export class BmvClustering extends Component {
     this.map = map
     window.map = map
 
-    map.addLayer({
-      id: 'clusters',
-      type: 'circle',
-      source: 'earthquakes',
-      filter: ['has', 'point_count'],
-      paint: {
-        // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-        // with three steps to implement three types of circles:
-        //   * Blue, 20px circles when point count is less than 100
-        //   * Yellow, 30px circles when point count is between 100 and 750
-        //   * Pink, 40px circles when point count is greater than or equal to 750
-        'circle-color': [
-          'step',
-          ['get', 'point_count'],
-          '#51bbd6',
-          100,
-          '#f1f075',
-          750,
-          '#f28cb1'
-        ],
-        'circle-radius': ['step', ['get', 'point_count'], 20, 100, 30, 750, 40]
-      }
-    })
-
-    map.addLayer({
-      id: 'cluster-count',
-      type: 'symbol',
-      source: 'earthquakes',
-      filter: ['has', 'point_count'],
-      layout: {
-        'text-field': '{point_count_abbreviated}',
-        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
-        'text-size': 12
-      }
-    })
-
-    map.addLayer({
-      id: 'unclustered-point',
-      type: 'circle',
-      source: 'earthquakes',
-      filter: ['!', ['has', 'point_count']],
-      paint: {
-        'circle-color': '#11b4da',
-        'circle-radius': 4,
-        'circle-stroke-width': 1,
-        'circle-stroke-color': '#fff'
-      }
-    })
-
     map.on('click', 'clusters', function(e) {
       var features = map.queryRenderedFeatures(e.point, {
         layers: ['clusters']
@@ -159,10 +110,59 @@ export class BmvClustering extends Component {
           onStyleLoad={this.onStyleLoad}
           zoom={zoom}
         >
-          <div>
-            <Source id="earthquakes" geoJsonSource={GEOJSON_SOURCE_OPTIONS} />
-          </div>
           {baseLayers()}
+
+          <Source id="earthquakes" geoJsonSource={GEOJSON_SOURCE_OPTIONS} />
+
+          <Layer
+            id="unclustered-point"
+            type="circle"
+            sourceId="earthquakes"
+            filter={['!', ['has', 'point_count']]}
+            paint={{
+              'circle-color': '#11b4da',
+              'circle-radius': 4,
+              'circle-stroke-width': 1,
+              'circle-stroke-color': '#fff'
+            }}
+          />
+          <Layer
+            id="clusters"
+            type="circle"
+            sourceId="earthquakes"
+            filter={['has', 'point_count']}
+            paint={{
+              'circle-color': [
+                'step',
+                ['get', 'point_count'],
+                '#51bbd6',
+                100,
+                '#f1f075',
+                750,
+                '#f28cb1'
+              ],
+              'circle-radius': [
+                'step',
+                ['get', 'point_count'],
+                20,
+                100,
+                30,
+                750,
+                40
+              ]
+            }}
+          />
+          <Layer
+            id="cluster-count"
+            type="symbol"
+            sourceId="earthquakes"
+            filter={['has', 'point_count']}
+            layout={{
+              'text-field': '{point_count_abbreviated}',
+              'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+              'text-size': 12
+            }}
+          />
         </Map>
       </Container>
     )
