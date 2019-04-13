276142fa9f38e848f5e3b27b5fd41b8268de0223
Scott McDonald <scottocorp@gmail.com>
2019-04-13 16:21:23 +1000

    add cluster click event

--- client/src/components/BmvClustering/index.js
+++ client/src/components/BmvClustering/index.js
@@ -124,6 +124,30 @@ export class BmvClustering extends Component {
       }
     })
 
+    map.on('click', 'clusters', function(e) {
+      var features = map.queryRenderedFeatures(e.point, {
+        layers: ['clusters']
+      })
+      var clusterId = features[0].properties.cluster_id
+      map
+        .getSource('earthquakes')
+        .getClusterExpansionZoom(clusterId, function(err, zoom) {
+          if (err) return
+
+          map.easeTo({
+            center: features[0].geometry.coordinates,
+            zoom: zoom
+          })
+        })
+    })
+
+    map.on('mouseenter', 'clusters', function() {
+      map.getCanvas().style.cursor = 'pointer'
+    })
+    map.on('mouseleave', 'clusters', function() {
+      map.getCanvas().style.cursor = ''
+    })
+
     this.render()
   }
   render() {
